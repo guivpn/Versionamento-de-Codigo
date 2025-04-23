08e1d1a2c62b5e79c49d4c9bc08190021d00c697
Leonardo Gabriel <leonardogabrielsilva1@outlook.com>
2025-04-23 09:46:36 -0300

    feat: criacao classe cliente

---
+++ Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente {
+
+	private Long id;
+	private String nome;
+	private String documento; // CPF ou CNPJ
+	private String email;
+	private String telefone;
+
+	public Cliente() {
+	}
+
+	public Cliente(String nome, String documento, String email, String telefone) {
+		this.id = id;
+		this.nome = nome;
+		this.documento = documento;
+		this.email = email;
+		this.telefone = telefone;
+	}
+}
